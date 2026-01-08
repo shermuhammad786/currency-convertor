dcddae6a6ac94d11b1faf7d386d3fac3cd8b3fc4
shermuhammad786 <sherabro141@gmail.com>
2026-01-08 15:13:14 +0500

    set env file

--- currency-frontend/src/services/axios.ts
+++ currency-frontend/src/services/axios.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  // baseURL: import.meta.env.VITE_API_BASE_URL,
-  baseURL: 'https://currency-convertor-theta-self.vercel.app/',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
