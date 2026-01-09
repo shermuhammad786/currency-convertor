6911b8585e5425fe78f5e0dac6b187f7dd707fbe
shermuhammad786 <sherabro141@gmail.com>
2026-01-09 14:56:50 +0500

    complete project

--- currency-frontend/src/features/currency/services/currency.api.ts
+++ currency-frontend/src/features/currency/services/currency.api.ts
@@ -1,8 +1,7 @@
-import axios from 'axios';
 import { api } from '../../../services/axios';
 import type { ConvertDto } from '../currency.types';
 
-export const convertCurrency = async(payload: ConvertDto) =>
- await axios.post('https://currency-convertor-backend-110.vercel.app/currency/convert', payload);
+export const convertCurrency = (payload: ConvertDto) =>
+  api.post('/currency/convert', payload);
 
 export const getHistory = () => api.get('/history');
