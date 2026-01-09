40f61a236d1c8b7647cde2c869b0812c0ed2612a
shermuhammad786 <sherabro141@gmail.com>
2026-01-09 15:28:34 +0500

    added README.md files

--- currency-frontend/eslint.config.js
+++ currency-frontend/eslint.config.js
@@ -4,7 +4,6 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
-
 export default defineConfig([
   globalIgnores(['dist']),
   {
@@ -20,4 +19,4 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+]);
